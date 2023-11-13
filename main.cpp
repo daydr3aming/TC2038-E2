e416bf21553d7109cce1ee9c13ff2703afcb0110
Roger <rogelioguzman73@gmail.com>
2023-11-13 00:08:10 -0600

    falta geometria nomas siu

--- main.cpp
+++ main.cpp
@@ -13,50 +13,58 @@
 using namespace std;
 
 // E2. Actividad Integradora 2
+// Situacion Problema: Ayudar a una empresa a incursionarse en los servicios de internet
 // Rogelio Guzman Cruzado - A01639914
 // David Alejandro González Ibarra - A01633817 
 
 // Algoritmo de Prim para MST. Complejidad: O(V^2)
-// Crea un solo arbol en vez de crear un bosque como Kruskal
+// La complejidad temporal puede reducirse a O(ElogV) si el grafo a leer se representa con una lista de adyacencia
+// Debido a que en una lista de adyacencia importa mas el numero de E que de V
 void MST(vector<vector<int>> nbhs){
     int V = nbhs.size();
     vector<int> key(V, INT_MAX);
     vector<int> parent(V, -1);
     vector<bool> inMST(V, false);
 
+    // Q <- 0
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
+    // DECREASE KEY (Q, u, 0)
     key[0] = 0;
 
+    // INSERT (Q, u)
     pq.push({0, 0});
 
+    // Encontramos los light edges 
     while (!pq.empty()){
+        // Extraer minimo key value
         int u = pq.top().second;
         pq.pop();
         inMST[u] = true;
-
-        // Exploramos todo lo adjacente a 
         for (int v = 0; v < V; ++v) {
             if (!inMST[v] && nbhs[u][v] < key[v]) {
-                key[v] = nbhs[u][v];
-                parent[v] = u;
-                pq.push({key[v], v});
+                key[v] = nbhs[u][v]; // DECREASE KEY
+                parent[v] = u; 
+                pq.push({key[v], v}); // DECREASE KEY
             }
         }
     }
     for (int i = 1; i < V; ++i) {
-        cout << "(" << parent[i] << "," << i << ")" << " KM: " <<nbhs[i][parent[i]] << endl;
+        cout << "(" << parent[i] << "," << i << ")" << endl;
     }
 }
 
-// Algoritmo de TSP con DP (Held Karp). Complejidad: O(2^n * n^2)
-// Debido a la naturaleza NP-Hard del problema, no existe un algoritmo que lo resuelva en tiempo polinomial
+// Algoritmo de TSP con DP (Held-Karp). Complejidad: O(2^n * n^2)
+// Debido a la naturaleza NP-Hard del problema, no existe un algoritmo que de una solucion exacta o optima en tiempo polinomial
 // Por lo que utilizaremos un approach de programacion dinamica para reducir la complejidad factorial de la fuerza bruta a exponencial
-// Held Karp da una solucion exacta y optima al problema de TSP pero en complejidad temporal muy elevada, por lo que sirve para entradas pequeñas
-// En este caso esperamos entradas y grafos pequeños por lo que consideramos que es una buena opcion
+// Held Karp da una solucion exacta y optima al problema de TSP pero con una complejidad temporal muy elevada, por lo que nos sirve entradas "pequeñas"
+// En este caso esperamos casos de prueba con vecindarios < 25, por lo que Held-Karp es relativamente aceptable
 int tsp_dp(vector<vector<int>>& nbhs, int mask, int pos, vector<vector<int>>& dp, vector<vector<int>>& parent, int n) {
+    // En los algoritmos de TSP es mas comun guardar los nodos visitados en una mascara de bits
+    // Esto para tener la mayor eficiencia en memoria y computacional posible debido a la intensidad de este problema
+    // en la mascara de bits el numero en i representa si la ciudad i fue visitada o no dependiendo de si es 1 o 0
     if (mask == (1 << n) - 1) {
-        return nbhs[pos][0];  // Return to the starting city
+        return nbhs[pos][0];  // Regresamos a la ciudad de inicio
     }
 
     if (dp[mask][pos] != -1) {
@@ -75,17 +83,16 @@ int tsp_dp(vector<vector<int>>& nbhs, int mask, int pos, vector<vector<int>>& dp
             }
         }
     }
-
-    parent[mask][pos] = next_city;  // Record the next city for reconstruction
+    parent[mask][pos] = next_city;  // Guardamos la ruta
     dp[mask][pos] = ans;
     return ans;
 }
 
 void printRoute(vector<vector<int>>& parent, int start, int n) {
-    int mask = 1;  // Start with the mask for the initial state
+    int mask = 1;  // Mascara de bits para el estado inicial
     int pos = start;
 
-    cout << "Ruta tomada: " << char(start + 'A') << " -> ";
+    cout << "Con ruta: " << char(start + 'A') << " -> ";
 
     while (mask != (1 << n) - 1) {
         int next_city = parent[mask][pos];
@@ -98,7 +105,7 @@ void printRoute(vector<vector<int>>& parent, int start, int n) {
 }
 
 // La aplicacion de Edmonds-Karp necesita un BFS para encontrar los caminos mas cortos o augmenting paths
-// Complejidad: O(V^2) por que usamos una matriz de adyacencia
+// Complejidad: O(V^2)
 bool bfs(vector<vector<int>>& residualGraph, vector<int>& parent, int source, int sink) {
     int vertices = residualGraph.size();
     vector<bool> visited(vertices, false);
@@ -161,24 +168,22 @@ void Geom(){
 
 int main() {
 
-    // Inicializamos n
+    // Numero de vecindarios
     int n;
     ifstream file("input.txt");
     file >> n;
 
-    // Esta matriz para TSP y MST
+    // Matriz de vecindarios donde cada conexion representa la distancia en km entre dos vecindarios
     vector<vector<int>> nbhs(n, vector<int>(n));
     for(int i = 0; i < n; i++){
-        cout << endl;
         for(int j = 0; j < n; j++){
             file >> nbhs[i][j];
         }
     }
 
-    // Esta matriz para el algoritmo del flujo maximo
+    // Matriz de vecindarios donde cada conexion representa el flujo maximo de fibra entre dos vecindarios
     vector<vector<int>> maxData(n, vector<int>(n));
     for(int i = 0; i < n; i++){
-        cout << endl;
         for(int j = 0; j < n; j++){
             file >> maxData[i][j];
         }
@@ -202,30 +207,29 @@ int main() {
 
 
     // MST Prim Salida 1
-    cout << "Cableado de todas las colonias (MST): " << endl;
+    cout << "Conectar todas las colonias con fibra: " << endl;
     MST(nbhs);
+    cout << endl;   
 
     vector<vector<int>> dp(1 << n, vector<int>(n, -1));
     vector<vector<int>> parent(1 << n, vector<int>(n, -1));
 
     // DP TSP Salida 2
-
     int min_path = tsp_dp(nbhs, 1, 0, dp, parent, n);  // La mask representa las ciudades visitadas, implementacion con mascaras de bits
 
-    cout << "Km minimos para visitar todas las colonias y volver al origen: " << endl;
+    cout << "Kilometros minimos para visitar todas las colonias y volver al origen: " << endl;
     cout << min_path << endl;
 
     int start = 0;  // Assuming the start vertex is 0 (A)
     printRoute(parent, start, n);
-
-    // Fin Salida 2
+    cout << endl;
 
     // Flujo Maximo Salida 3
     int source = 0;
     int sink = 3;
 
     int maxFlow = MaxFlow(maxData, source, sink);
-    cout << "Flujo maximo de: " << source << " a " << sink << endl;
+    cout << "Flujo maximo de " << source << " a " << sink << endl;
     cout << maxFlow << endl;
 
     return 0;
